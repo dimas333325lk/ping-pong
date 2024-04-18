21ba77f9265eacccfd5e83a409b011d73bcdd2b9
dimas333325lk <149696655+dimas333325lk@users.noreply.github.com>
2024-04-18 18:11:03 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,7 @@
 from pygame import *
+from random import *
 mixer.init()
+font.init()
 udar = mixer.Sound('udar.ogg')
 
 class GameSprite(sprite.Sprite):
@@ -27,24 +29,47 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 game=True
+font1 = font.SysFont('Verdana',35)
+lose2 = font1.render('Красный игрок пропустил мяч',True,(0,255,0))
+lose1 = font1.render('Зелёный игрок пропустил мяч',True,(255,0,0))
+finish = False
+ballx=3
+bally=3
+klk = 0
 bg = transform.scale(image.load('table.png'),(700,500))
-playerone = Player('rocketkaone.png',5,0,5,50,200)
-playertwo = Player('rocketkatwo.png',5,650,5,50,200)
+playerone = Player('rocketkaone.png',5,650,5,50,200)
+playertwo = Player('rocketkatwo.png',5,0,5,50,200)
 ball = Player('myach.png',5,350,250,50,50)
 window = display.set_mode((700,500))
 window.blit(bg,(0,0))
 clock = time.Clock()
 
 while game:
-    window.blit(bg,(0,0))
-    ball.reset()
-    playerone.reset()
-    playertwo.reset()
-    playerone.update_right()
-    playertwo.update_left()
     for e in event.get():
         if e.type == QUIT:
             game =False
+    if finish != True:
+        window.blit(bg,(0,0))
+        ball.rect.x += ballx
+        ball.rect.y += bally
+        if ball.rect.y > 450 or ball.rect.y <0:
+            bally *= -1
+            udar.play()
+        elif sprite.collide_rect(playerone,ball) or sprite.collide_rect(playertwo,ball):
+            udar.play()
+            ballx *= -1
+        elif ball.rect.x <0:
+            window.blit(lose1,(0,200))
+            finish = True
+        elif ball.rect.x >650:
+            window.blit(lose2,(0,200))
+            finish = True
+        
+        ball.reset()
+        playerone.reset()
+        playertwo.reset()
+        playerone.update_right()
+        playertwo.update_left()
     clock.tick(60)
-    udar.play()
     display.update()
+    
